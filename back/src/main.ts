b2279b84664f5d9f86701c5b8e152cf272f14f00
Rian Pedro <rian.ace.2003@gmail.com>
2024-11-13 20:16:03 -0300

    test

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import router from "./routes"
 import path from "path"
+import cors from "cors"
 
 const app = express()
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(cors())
 app.use(express.static(path.join(__dirname, "..","..","front","TreeWise","dist")))
 
 app.use(router)
